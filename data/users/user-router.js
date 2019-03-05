92891ce20460288ea0af5f1c7a2084bd2b4e2d74
badCompany55 <zachirvin45@gmail.com>
2019-03-05 07:26:44 -0600

    SINGLE USER add get request for single user by id

--- data/users/user-router.js
+++ data/users/user-router.js
@@ -7,7 +7,18 @@ router.get("/", async (req, res) => {
     const theUsers = await users.get();
     res.status(200).json(theUsers);
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: "Failed to get all users" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const theUser = await users.getById(req.params.id);
+    res.status(200).json(theUser);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: `The user with id ${req.params.id} does not exisit` });
   }
 });
 
