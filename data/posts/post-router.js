646d0e9617a266186e1519115b7c9ccaf74eb483
badCompany55 <zachirvin45@gmail.com>
2019-03-05 19:39:54 -0600

    GET/POSTS add get ability for posts

--- data/posts/post-router.js
+++ data/posts/post-router.js
@@ -3,3 +3,12 @@ const posts = require("../helpers/postDb.js");
 const router = express.Router();
 
 module.exports = router;
+
+router.get("/", async (req, res) => {
+  try {
+    const allPosts = await posts.get();
+    res.status(200).json({ posts: allPosts });
+  } catch {
+    res.status(500).json({ error: "Unable to get the requested posts" });
+  }
+});
