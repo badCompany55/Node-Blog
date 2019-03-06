965b15ece52ebbaa812e59fc622e916ed30776e5
badCompany55 <zachirvin45@gmail.com>
2019-03-05 20:05:58 -0600

    UPDATE/DELETE/POSTS add update post ability add delete post ability

--- data/posts/post-router.js
+++ data/posts/post-router.js
@@ -23,3 +23,30 @@ router.get("/:id", async (req, res) => {
       .json({ error: `Unable to get post with the id of ${req.params.id}` });
   }
 });
+
+router.post("/", async (req, res) => {
+  try {
+    const newPost = await posts.insert(req.body);
+    res.status(201).json(newPost);
+  } catch {
+    res.status(500).json({ error: "Unable to add new post" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedPost = await posts.update(req.params.id, req.body);
+    res.status(200).json(updatedPost);
+  } catch {
+    res.status(500).json({ error: "Unable to update post" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletePost = await posts.remove(req.params.id);
+    res.status(202).json(deletePost);
+  } catch {
+    res.status(500).json({ error: "Unable to delete post" });
+  }
+});
