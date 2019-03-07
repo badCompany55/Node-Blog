4a0837cc762234897417f1ba5c51a6f2ae923d95
badCompany55 <zachirvin45@gmail.com>
2019-03-06 22:13:01 -0600

    process

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const server = require("./server.js");
 
-const PORT = 4000;
-
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`/n *** Server Running on http://localhost:${PORT}`);
 });
